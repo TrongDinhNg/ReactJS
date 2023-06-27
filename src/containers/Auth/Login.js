8b24ae453b526ba25349df5370fd944e5a16cb13
Trong <tronglong28@gmail.com>
2023-06-27 15:58:41 +0700

    Form Login-handle the onClick and onChange events

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -9,7 +9,31 @@ import { FormattedMessage } from "react-intl";
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: "",
+            password: "",
+            isShowPassword: false,
+        };
     }
+    handlerOnchangeUsername = (even) => {
+        this.setState({
+            username: even.target.value,
+        });
+    };
+    handlerOnchangePassword = (even) => {
+        this.setState({
+            password: even.target.value,
+        });
+    };
+    handlerLogin = () => {
+        alert(this.state);
+        console.log(this.state);
+    };
+    handlerShowHidePassWord = () => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword,
+        });
+    };
 
     render() {
         return (
@@ -18,26 +42,54 @@ class Login extends Component {
                     <div className="login-content row">
                         <div className="col-12 text-login">Login</div>
                         <div className="col-12 form-group login-input">
-                            <label>UserName</label>
+                            <label>Username</label>
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Enter your username"
+                                value={this.state.username}
+                                onChange={(even) =>
+                                    this.handlerOnchangeUsername(even)
+                                }
                             />
                         </div>
                         <div className="col-12 form-group login-input">
                             <label>Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Enter your password "
-                            />
+                            <div className="custom-input-password">
+                                <input
+                                    type={
+                                        this.state.isShowPassword
+                                            ? "text"
+                                            : "password"
+                                    }
+                                    className="form-control"
+                                    placeholder="Enter your password "
+                                    value={this.state.password}
+                                    onChange={(even) =>
+                                        this.handlerOnchangePassword(even)
+                                    }
+                                />
+                                <div
+                                    onClick={() =>
+                                        this.handlerShowHidePassWord()
+                                    }
+                                >
+                                    <i
+                                        class={
+                                            this.state.isShowPassword
+                                                ? "fas fa-eye"
+                                                : "fas fa-eye-slash"
+                                        }
+                                    ></i>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-12 ">
                             <input
                                 className="btn-login"
                                 type="submit"
                                 value="Log in"
+                                onClick={() => this.handlerLogin()}
                             />
                         </div>
                         <div className="col-12">
@@ -45,7 +97,7 @@ class Login extends Component {
                                 Fogot your password?
                             </span>
                         </div>
-                        <div className="col-12 mt-3">
+                        <div className="col-12 mt-5">
                             <span>Or sign in with:</span>
                         </div>
                         <div className="col-12 social-login">
