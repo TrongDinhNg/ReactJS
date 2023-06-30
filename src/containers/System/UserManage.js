7173443b5e9f7c4633e4cfe6c8e5f385febc9530
Trong <tronglong28@gmail.com>
2023-06-30 18:37:32 +0700

    React_CRUD_read_display users

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,97 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import userService from "../../services/userService";
+import "./UserManage.scss";
+
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await userService.getAllUsers("ALL");
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.user,
+            });
+        }
     }
 
-
+    /**
+     * Lifecycle
+     * Run Component
+     * 1_ run contructor => init state
+     * 2_Did Mount(set state)
+     * 2_render
+     *
+     */
     render() {
+        // console.log("check Render", this.state);
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center"> User Manage</div>
+                <div className="users-table mt-3 mx-1">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">FirstName</th>
+                                <th scope="col">LastName</th>
+                                <th scope="col">Address</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arrUsers &&
+                                arrUsers.map((item, index) => {
+                                    return (
+                                        <>
+                                            <tr>
+                                                <th scope="row">{index + 1}</th>
+                                                <td>{item.email}</td>
+                                                <td>{item.firstName}</td>
+                                                <td>{item.lastName}</td>
+                                                <td>{item.address}</td>
+                                                <td>
+                                                    <a
+                                                        type="button"
+                                                        className="btn btn-edit btn-primary"
+                                                        href="/"
+                                                    >
+                                                        <i className="fas fa-edit"></i>
+                                                    </a>
+                                                    <a
+                                                        type="button"
+                                                        className="btn btn-delete btn-primary"
+                                                        href="/"
+                                                    >
+                                                        <i className="fas fa-trash"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    );
+                                })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,5 +4,9 @@ const userService = {
     login(email, password) {
         return axios.post("/api/login", { email, password });
     },
+
+    getAllUsers(inputId) {
+        return axios.get(`/api/get-all-users?id=${inputId}`);
+    },
 };
 export default userService;
